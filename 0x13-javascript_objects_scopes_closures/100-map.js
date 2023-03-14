d45eb7d307f4805bcf3b429657cb08e37e5cbd34
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-03-14 14:43:09 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.list = [1, 2, 3, 4, 5];
-const arr = require('./100-data').list;
 
+const arr = require('./100-data').list;
+exports.list = [1, 2, 3, 4, 5];
 console.log(arr);
 console.log(arr.map((x, idx) => x * idx));
