88eeafbb1d7957e859bd472e8fb14af7d4e23041
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-04-25 21:04:13 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-# Ask the user for the file name
-filename = input("Enter the name of the file: ")
 
-# Open the file in read mode
-with open(filename, 'r') as file:
-    # Read the contents of the file
-    content = file.read()
-    # Print the contents of the file
-    print(content)
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
