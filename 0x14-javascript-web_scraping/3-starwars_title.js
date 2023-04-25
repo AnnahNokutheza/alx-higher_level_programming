ed184ae8d0496958a82ae8fcd174ea8e7bb4b07b
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-04-25 21:21:08 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 
-import sys
-import requests
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-movie_id = sys.argv[1]
-response = requests.get(f"https://swapi-api.alx-tools.com/api/films/{movie_id}")
-
-if response.status_code == 200:
-    movie = response.json()
-    if int(movie["episode_id"]) == int(sys.argv[2]):
-        print(movie["title"])
-else:
-    print("Error retrieving data from API.")
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
