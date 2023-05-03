4876206901490432e7e51426e2053ed39e4e66b9
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-05-03 20:01:39 +0200

    Create 3-script.js

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+// Attach a click event handler to the red_header element using jQuery
+$('#red_header').click(function() {
+  // Get the header element using jQuery
+  const header = $('header');
+  
+  // Add the red class to the header element
+  header.addClass('red');
+});
